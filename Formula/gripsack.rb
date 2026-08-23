b7521e28891ec12658ab24713a53e2e797e8f3a6
tknawara <tarek.nawara@gmail.com>
2026-08-23 10:12:53 +0100

    gripsack 0.1.1

--- Formula/gripsack.rb
+++ Formula/gripsack.rb
@@ -1,8 +1,8 @@
 class Gripsack < Formula
   desc "Your whole environment in one bag"
   homepage "https://gripsack.dev"
-  url "https://static.crates.io/crates/gripsack/gripsack-0.1.0.crate"
-  sha256 "328145c00702ec88d5aa4768b4ea32a37bc598f9c3f6ed12ba5f2ff02cda2f66"
+  url "https://static.crates.io/crates/gripsack/gripsack-0.1.1.crate"
+  sha256 "337d35cb6eac1d6dcc971e8fff5aa26e2c09664410f438bdb3629c5038cb4fbb"
   license "MIT"
 
   depends_on "rust" => :build
